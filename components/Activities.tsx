c8b59d737b4d65ebbd9514a087a47b63bce5e9f7
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2021-11-05 14:21:57 +0200

    LoginPage design done

--- components/Activities.tsx
+++ components/Activities.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from '../styles/Activities.module.css';
 import Activity from './Activity';
 const Activiries = () => {
+  const [color, setColor] = React.useState('');
+  console.log(color);
   return (
     <section className={styles.activities__container}>
       <Activity />
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import Link from 'next/link';
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
+import styles from '../styles/Login.module.css';
+import img from '../public/PageImages/Login.jpg';
+import Image from 'next/image';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state';
@@ -8,6 +12,7 @@ import { actionCreators } from '../state';
 const login = () => {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [show, setShow] = React.useState(false);
   const dispatch = useDispatch();
   const { loginUser } = bindActionCreators(actionCreators, dispatch);
   const loginUserLocal = async () => {
@@ -26,38 +31,79 @@ const login = () => {
       .catch((err) => console.log(err));
   };
   return (
-    <div>
-      <h1>Login page</h1>
-      <Link href='/'>go back</Link>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          loginUserLocal();
-        }}
-      >
-        <div>
-          <h2>Email:</h2>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <section className={styles.login__page}>
+      <div className={styles.login__container}>
+        <div className={styles.login__form__container}>
+          <h1>Account Login</h1>
+          {/* <Link href='/'>go back</Link> */}
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              loginUserLocal();
+            }}
+            className={styles.login__form}
+          >
+            <div className={styles.input__wrapper}>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                value={email}
+                className={styles.input}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email...'
+              />
+            </div>
+            <div className={styles.input__wrapper}>
+              <input
+                type={show ? 'text' : 'password'}
+                name='password'
+                id='password'
+                value={password}
+                className={styles.input}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password...'
+              />
+              {/* <button
+                className={styles.show__password}
+                onClick={() => setShow(!show)}
+              >
+                {' '}
+              </button> */}
+              {show ? (
+                <AiOutlineEye
+                  fill='black'
+                  className={styles.show__password}
+                  onClick={() => setShow(!show)}
+                />
+              ) : (
+                <AiOutlineEyeInvisible
+                  fill='black'
+                  className={styles.show__password}
+                  onClick={() => setShow(!show)}
+                />
+              )}
+            </div>
+            <div className={styles.remember__password}>
+              <input type='checkbox' id='remember' />
+              <label htmlFor='remember'> Remember Me?</label>
+            </div>
+            <button className={styles.form__submit} type='submit'>
+              Login
+            </button>
+          </form>
+          <p>
+            Dont have an account?{' '}
+            <Link href='/register' passHref>
+              <a className={styles.link}> Register now.</a>
+            </Link>
+          </p>
         </div>
-        <div>
-          <h2>password:</h2>
-          <input
-            type='password'
-            name='password'
-            id='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <div className={styles.login__image__container}>
+          <Image className={styles.login__image} src={img} />
         </div>
-        <button type='submit'>Login</button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
 
